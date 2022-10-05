c46c963cd5c9c14b19707eda99123b099223b55d
dev-choee <rhksgh99@gmail.com>
2022-10-05 13:28:19 +0900

    스프링 데이터 JPA 로 구현

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -11,25 +11,28 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    //private final DataSource dataSource;
+    //private final EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    @Autowired // 생성자가 하나인 경우는 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        //this.dataSource = dataSource;
+        //this.em = em;
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+    //@Bean
+    //public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
         // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    //}
 }
